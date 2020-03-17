3545e8778c99bc21c6844c3d58edb2aa9328dbfb
MSereez <60780094+MSereez@users.noreply.github.com>
2020-03-17 12:10:54 +0000

    Create vectorFields.h

---
+++ src/finiteDifference/vectorFields.h
@@ -0,0 +1,13 @@
+#ifndef VECTORFIELDS_H
+#define VECTORFIELDS_H
+
+class vectorFields
+{
+public:
+    vectorFields();
+    virtual ~vectorFields();
+    double value;
+    double X,Y;
+};
+
+#endif // VECTORFIELDS_H
