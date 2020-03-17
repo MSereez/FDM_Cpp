29f3fde9b3204c493f1a12fdb112cfb89b827db8
MSereez <60780094+MSereez@users.noreply.github.com>
2020-03-17 12:15:19 +0000

    Update fileWriter.h

--- src/misc/fileWriter.h
+++ src/misc/fileWriter.h
@@ -1,153 +1,30 @@
-#include "fieldOperations.h"
+#ifndef FILEWRITER_H
+#define FILEWRITER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include "Fields.h"
+#include <cstring>
+#include <sstream>
+using namespace std;
+using std::vector;
+using std::string;
 
-fieldOperations::fieldOperations()
+
+
+class fileWriter
 {
-}
+public:
+    fileWriter();
+    
+    virtual ~fileWriter();
+    
+    
+    void write(string&, Mesh&, Fields::volVectorField&, Fields::volVectorField& , Fields::volScalarField&,int  );
+    
+};
 
-fieldOperations::~fieldOperations()
-{
-}
-
-
-/*
-Fields::volVectorField operator+(const Fields::volVectorField& A,const Fields::volVectorField& B)
- {
-     Fields::volVectorField result(A);
-         forAllInternal(A)
-         {
-                    result[i][j].value += B[i][j].value;
-             }
-         
-     return result;
- }
-
- Fields::volVectorField operator+(const Fields::volVectorField& A,const Fields::volScalarField& B)
- {
-     Fields::volVectorField result(A);
-         forAllInternal(A)
-         {
-                    result[i][j].value += B[i][j].value;
-             }
-       
-     return result;
- }
- 
-   Fields::volScalarField operator+(const Fields::volScalarField& A,const Fields::volVectorField& B)
- {
-     Fields::volScalarField result(A);
-         forAllInternal(A)
-         {
-                    result[i][j].value += B[i][j].value;
-             }
-       
-     return result;
- }
- Fields::volScalarField operator+(const Fields::volScalarField& A,const Fields::volScalarField& B)
- {
-     Fields::volScalarField result(A);
-         forAllInternal(A)
-         {
-                    result[i][j].value += B[i][j].value;
-             }
-       
-     return result;
- }*/
- 
-/*
-// cross product
- Fields::volVectorField operator&&(const Fields::volVectorField& A,const Fields::volVectorField& B)
-    {
-          Fields::volVectorField result(A);
-         forAllInternal(A)
-         {
-                  result[i][j].value *= B[i][j].value;
-             }
-       
-        return result;
-    }
-
- Fields::volVectorField operator&&(const Fields::volVectorField& A,const Fields::volScalarField& B)
-    {
-          Fields::volVectorField result(A);
-         forAllInternal(A)
-         {
-                  result[i][j].value *= B[i][j].value;
-             }
-       
-        return result;
-    }
-      Fields::volVectorField operator==(const Fields::volVectorField& A, const Fields::volVectorField& B)
-      {
-          Fields::volVectorField result(A);
-         forAllInternal(A)
-         {
-                  result[i][j].value = B[i][j].value;
-             }
-       
-        return result;
-    }
-          Fields::volVectorField operator==(const Fields::volVectorField& A, const Fields::volScalarField& B)
-      {
-          Fields::volVectorField result(A);
-         forAllInternal(A)
-         {
-                  result[i][j].value = B[i][j].value;
-             }
-       
-        return result;
-    }
-
-
- Fields::volVectorField operator-(const Fields::volVectorField& A,const Fields::volVectorField& B)
- {
-     Fields::volVectorField result(A);
-         forAllInternal(A)
-         {
-                    result[i][j].value -= B[i][j].value;
-             }
-         
-     return result;
- }
-  Fields::volScalarField operator-(const Fields::volScalarField& A,const Fields::volVectorField& B)
- {
-     Fields::volScalarField result(A);
-         forAllInternal(A)
-         {
-                    result[i][j].value -= B[i][j].value;
-             }
-         
-     return result;
- }
- 
-   Fields::volVectorField operator-(const Fields::volVectorField& A,const Fields::volScalarField& B)
- {
-     Fields::volVectorField result(A);
-         forAllInternal(A)
-         {
-                    result[i][j].value -= B[i][j].value;
-             }
-         
-     return result;
- }
-     Fields::volVectorField operator*(double& dblvalue, const Fields::volVectorField& A)
-    {
-         Fields::volVectorField result(A);
-         forAllInternal(A)
-         {
-                   result[i][j].value *=  dblvalue ;
-             }
-       
-       return result;
-    }   
-
-
- Fields::volScalarField operator*(double& dblvalue, const Fields::volScalarField& A)
-    {
-         Fields::volScalarField result(A);
-         forAllInternal(A)
-         {
-                   result[i][j].value *=  dblvalue ;
-             }
-       
-       return result;
-    }   */
+#endif // FILEWRITER_H
