4be61146893124d8718534bac68c1a20bb33146e
MSereez <60780094+MSereez@users.noreply.github.com>
2020-03-17 12:10:25 +0000

    Create scalarFields.cpp

---
+++ src/finiteDifference/scalarFields.cpp
@@ -0,0 +1,13 @@
+#include "scalarFields.h"
+
+scalarFields::scalarFields() : value(0.0)
+{
+}
+scalarFields::scalarFields(const scalarFields& sca) : value(sca.value)
+{
+    
+}
+scalarFields::~scalarFields()
+{
+}
+
